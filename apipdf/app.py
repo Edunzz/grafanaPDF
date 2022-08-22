683dbfef356dd0412f5441a2a0f11b0ad2ca3617
JoseRomero616 <jo.seeduardo25397stk@gmail.com>
2022-08-22 12:04:47 -0500

    Update app.py

--- apipdf/app.py
+++ apipdf/app.py
@@ -3,8 +3,16 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
-    return jsonify({'status': 'launching rocket...'})
+def status():
+    return jsonify({'status': 'ok'})
+
+@app.route('/reporte/<uid>')
+def reporte(uid):
+    return(render_template("reporte_grafana.html",
+                           report_name=data_grafana["meta"]["slug"],
+                           from_date=dashboard_vars["from"],
+                           to_date=dashboard_vars["to"],
+                           data_render=data_render))
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=os.getenv('PORT'))
